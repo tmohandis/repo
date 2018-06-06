a5751eefca195708e5c45cffb8f8b984d700133f
tmohandis <ghostwo1f@mail.ru>
2018-06-06 17:18:19 +0300

    запрос на картинки с котиками

--- hw2/task3/js.js
+++ hw2/task3/js.js
@@ -2,10 +2,21 @@
 function request() {
     var xhr = new XMLHttpRequest();
     xhr.open('GET', 'http://127.0.0.1:8080/json.json');
+    xhr.send();
 
     xhr.onreadystatechange = function () {
         if (xhr.readyState === XMLHttpRequest.DONE) {
-            console.log(xhr.responseText);
+            var images = JSON.parse(xhr.responseText);
+            images.forEach(element => {
+                var image = document.createElement('img');
+                image.title = element.title;
+                image.src = element.icon;
+                var link = document.createElement('a');
+                link.href = element.full;
+                link.appendChild(image);
+                document.body.appendChild(link);
+
+            });
         }
     }
 }
